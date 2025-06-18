9fccc7dfcb6183651d5a1389f0822c26fef7ac3d
Kyle Simpson <Kyle_Simpson5624@yahoo.com>
2025-06-18 11:03:59 -0500

    Script to run benchmarks for SmartSet only and save json file

---
+++ scripts/run_smartset_benchmarks.py
@@ -0,0 +1,37 @@
+#
+# This script will run only smartset benchmarks in tests/smartset/test_benchmarks.py
+# It saves the results to .benchmarks/smartset_benchmark_<timestamp>.json
+# and prints the results out to the console.
+#
+
+import os
+import sys
+import subprocess
+from datetime import datetime
+from pathlib import Path
+
+def main():
+    # Add project root to PATH
+    sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
+
+    timestamp = datetime.now().strftime("%m%d%Y-%H%M")
+
+    root = Path(__file__).resolve().parents[1]
+    smartset_benchmarks = Path(root).joinpath('tests', 'smartset', 'test_benchmarks.py')
+    logs_dir = Path(root).joinpath('logs', '.benchmarks')
+    logs_dir.mkdir(parents=True, exist_ok=True)
+
+    if smartset_benchmarks.exists():
+        command = f'pytest {str(smartset_benchmarks)} --benchmark-only --benchmark-save=smartest_benchmark_{timestamp}.json --benchmark-storage={str(logs_dir)}'
+        try:
+            subprocess.run(command, shell=True, check=True)
+        except subprocess.CalledProcessError as e:
+            print(f'ERROR: Subprocess: {e}')
+
+if __name__ == "__main__":
+    try:
+        main()
+    except FileNotFoundError as e:
+        print(f'ERROR: Cannot find file: {e}')
+    except Exception as e:
+        print(f'ERROR: {e}')
